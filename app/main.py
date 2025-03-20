88af18e19676cd541ff02435d1e2dc4924b7faf1
jumailj <jumailj.work@proton.me>
2025-03-21 02:27:44 +0530

    change port to 10000

--- app/main.py
+++ app/main.py
@@ -263,5 +263,5 @@ if __name__ == "__main__":
     import uvicorn
     import os
 
-    port = int(os.getenv("PORT", 8000))
+    port = int(os.getenv("PORT", 10000))
     uvicorn.run(app, host="0.0.0.0", port=port)
